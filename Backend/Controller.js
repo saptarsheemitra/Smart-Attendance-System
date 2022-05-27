1cf95f20a61b9af23f308030732ae48bb76f12b6
vinit agarwal <vinitagarwal.garg@gmail.com>
2022-05-28 01:38:00 +0530

    sql to sqlite stack change

--- Backend/Controller.js
+++ Backend/Controller.js
@@ -1,40 +1,52 @@
 const req = require("express/lib/request");
 const mysql = require("mysql2")
-const dbcon = require("./dbcon");
+const dbcon = require("./dbcon").db;
 const moment = require("moment")
 
 function login(req,res){
     console.log(req.body)
     res.send("ok")
-    dbcon.execute("select * from userdb").then((result) => {
-        console.log(result[0]);
-    })
 }
 function markpresent(req,res){
     var roll = req.body.rollno
-    dbcon.execute(`insert into studentdb select '${roll}','${req.body.name}','${req.body.date}','present' where not exists(select * from studentdb where roll = '${roll}' and cast(mdate as date) = '${moment(req.body.date).format("YYYY-MM-DD")}')`).then((result)=>{
-        console.log(result[0].affectedRows);
-        if (result[0].affectedRows == 0) {
-            res.send(roll+" already marked")
+    dbcon.run(`insert into studentdb select '${roll}','${req.body.name}','${req.body.branch}','${req.body.date}','${req.body.time}','present' where not exists(select * from studentdb where roll = '${roll}' and mdate = '${moment(req.body.date).format("YYYY-MM-DD")}')`,(result,err)=>{
+        console.log(result);
+        if(err){
+            res.send("error: "+err)
         }else{
-        res.send(roll+" marked present")}
+            dbcon.all("select changes() as affectedrow",(err,row)=>{
+                console.log(row[0].affectedrow);
+                if (row[0].affectedrow == 0) {
+                    res.send(roll+" already marked")
+                }else{
+                res.send(roll+" marked present")}
+            })
+        }
     })
 
 }
 function markabsent(req,res){
     var roll = req.body.rollno
-    dbcon.execute(`insert into studentdb select '${roll}','${req.body.name}','${req.body.date}','absent' where not exists(select * from studentdb where roll = '${roll}' and cast(mdate as date) = '${moment(req.body.date).format("YYYY-MM-DD")}')`).then((result)=>{
-        if (result[0].affectedRows == 0) {
-            res.send(roll+" already marked")
+    dbcon.run(`insert into studentdb select '${roll}','${req.body.name}','${req.body.branch}','${req.body.date}','${req.body.time}','absent' where not exists(select * from studentdb where roll = '${roll}' and mdate = '${moment(req.body.date).format("YYYY-MM-DD")}')`,(result,err)=>{
+        console.log(result);
+        if(err){
+            res.send("error: "+err)
         }else{
-        res.send(roll+" marked absent")}
+            dbcon.all("select changes() as affectedrow",(err,row)=>{
+                console.log(row[0].affectedrow);
+                if (row[0].affectedrow == 0) {
+                    res.send(roll+" already marked")
+                }else{
+                res.send(roll+" marked present")}
+            })
+        }
     })
 
 }
 function getreport(req,res){
     var out = []
-    dbcon.execute(`select roll,name,cast(mdate as date) as date,cast(mdate as time) as time,status from studentdb where cast(mdate as date) = '${req.body.date}'`).then((result)=>{
-        result[0].forEach(element => {
+    dbcon.all(`select roll,branch,name,mdate as date,mtime as time,status from studentdb where mdate = '${req.body.date}'`,(err,rows)=>{
+        rows.forEach(element => {
             element.date = moment(element.date).format("DD-MM-YYYY")
             out.push(element)
         });
@@ -43,8 +55,8 @@ function getreport(req,res){
 }
 function getallreport(req,res){
     var out = []
-    dbcon.execute(`select roll,name,cast(mdate as date) as date,cast(mdate as time) as time,status from studentdb`).then((result)=>{
-        result[0].forEach(element => {
+    dbcon.all(`select roll,name,branch,mdate as date,mtime as time,status from studentdb`,(err,rows)=>{
+        rows.forEach(element => {
             element.date = moment(element.date).format("DD-MM-YYYY")
             out.push(element)
         });
--- Backend/dbcon.js
+++ Backend/dbcon.js
@@ -1,12 +1,12 @@
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
-dotenv.config();
-//vinit
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    database: "hackathon",
-    password: "vinitagarwal",
-});
 
-module.exports = pool.promise();
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./dbfile.db')
+db.run(`CREATE TABLE if not exists studentdb (
+    roll varchar(10) DEFAULT NULL,
+    name varchar(50) DEFAULT NULL,
+    branch varchar(50),
+    mdate date DEFAULT NULL,
+    mtime time,
+    status varchar(10) DEFAULT NULL
+  )`);
+ exports.db = db
